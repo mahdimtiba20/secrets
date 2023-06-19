957d89547670a14ac76b37d80343f338287dc04b
mtibamahdi <mtiba.mahdi@gmail.com>
2023-06-19 04:00:53 +0100

    bcrypt 10 round encryption

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+// const md5 = require("md5");
 // const encrypt = require('mongoose-encryption');
 
 const app = express();
@@ -36,33 +38,38 @@ app.get("/register", (req, res)=>{
 });
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-    newUser.save()
-        .then(()=>{
-            res.render('secrets');
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
         })
-        .catch((err) => {
-                console.log(err);
-        })
+        newUser.save()
+            .then(()=>{
+                res.render('secrets');
+            })
+            .catch((err) => {
+                    console.log(err);
+            })
+    }); 
 });
 
 app.post("/login", (req, res)=>{
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({
         email: username
     })
     .then((foundUser)=>{
         if (foundUser){
-            if (foundUser.password === password){
-                res.render("secrets");
-            }else{
-                res.send("Incorrect Password");
-            }
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if (result === true){
+                    res.render("secrets");
+                }else{
+                    res.send("Incorrect Password");
+                }
+            });
         }else{
             res.send("Email Doesn't Exist");
         }
