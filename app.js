e412e7d0037a407fd2711ed7388a9dcedacf5de9
mtibamahdi <mtiba.mahdi@gmail.com>
2023-06-19 19:49:46 +0100

    Level 6 Completed : passport-local-mongoose

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-// const md5 = require("md5");
-// const encrypt = require('mongoose-encryption');
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -15,18 +14,37 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 // Start Coding ///////////////////////////////////
 app.get("/", (req, res)=>{
-    res.render("home");
+    if (req.isAuthenticated()){
+        res.redirect("/secrets");
+    }else{
+        res.render("home");
+    } 
 });
 
 app.get("/login",(req, res)=>{
@@ -37,47 +55,54 @@ app.get("/register", (req, res)=>{
     res.render("register");
 });
 
+app.get("/secrets", (req, res)=>{
+    if (req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
+});
+
 app.post("/register", (req, res) => {
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        // Store hash in your password DB.
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        })
-        newUser.save()
-            .then(()=>{
-                res.render('secrets');
-            })
-            .catch((err) => {
-                    console.log(err);
-            })
-    }); 
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if (err){
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
 });
 
 app.post("/login", (req, res)=>{
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({
-        email: username
-    })
-    .then((foundUser)=>{
-        if (foundUser){
-            bcrypt.compare(password, foundUser.password, function(err, result) {
-                if (result === true){
-                    res.render("secrets");
-                }else{
-                    res.send("Incorrect Password");
-                }
-            });
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+            res.redirect("/login");
         }else{
-            res.send("Email Doesn't Exist");
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
         }
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-})
+    });
+});
+
+app.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/');
+    });
+  });
+  
+
 // End Coding ///////////////////////////////////
 app.listen(3000,()=>{
     console.log("** App Started on Port 3000");
